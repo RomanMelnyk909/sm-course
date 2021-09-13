4bf11a409bf7d693a22b7e7eaa85f9dd6b29b4e5
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2021-09-13 20:43:35 +0300

    add users and show it on the screen

---
+++ practicle-01/src/components/Users/AddUser.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredUserAge, setEnteredUserAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (
+      enteredUserName.trim().length === 0 ||
+      enteredUserAge.trim().length === 0
+    ) {
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      return;
+    }
+
+    props.onAddUser(enteredUserName, enteredUserAge);
+    setEnteredUserName("");
+    setEnteredUserAge("");
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const userAgeChangeHandler = (event) => {
+    setEnteredUserAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <ErrorModal title='An error occured!' message='Something went wrong'/>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">UserName</label>
+          <input
+            id="username"
+            type="text"
+            onChange={userNameChangeHandler}
+            value={enteredUserName}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={userAgeChangeHandler}
+            value={enteredUserAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
